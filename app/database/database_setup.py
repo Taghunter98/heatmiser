95c84029b4159d5d7a8f40e7704a235aa69d5d6a
Josh Bassett <bassettjosh397@gmail.com>
2025-03-06 12:59:05 +0000

    Server tests are successful, cleaned up code, merging with dev

--- app/database/database_setup.py
+++ app/database/database_setup.py
@@ -3,21 +3,19 @@ from dotenv import load_dotenv
 import os
 
 def connect():
-    # Get variables
+    # Load variables
     load_dotenv('app/.env')
 
     try:
         # Establish connection
-        database = mysql.connector.connect(
+        connection = mysql.connector.connect(
             host = os.getenv('HOST'),
             user = os.getenv('USER'),
             password = os.getenv('PASS'),
             database = os.getenv('DATABASE')
         )
-        print("Connection is successful")
-        return database
+
+        return connection
     except mysql.connector.Error as e:
         print(f"Error: {e}")
         return -1
-    
-connect()
--- app/tests/db_test.py
+++ app/tests/db_test.py
@@ -4,7 +4,8 @@ from app.database import database_setup
 class TestDatabase(unittest.TestCase):
     def testConnection(self):
         connection = database_setup.connect()
-        self.assertNotEqual(connection, -1)
+        # Check connection, if wrong it will return -1
+        self.assertNotEqual(connection, -1, "Connection is not working")
 
 if __name__ == "__main__":
     unittest.main()
