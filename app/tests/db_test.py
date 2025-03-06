623558a091cccabdcb117df02abda523e81f3d66
Josh Bassett <bassettjosh397@gmail.com>
2025-03-06 13:45:20 +0000

    Updated database unit test

--- app/tests/db_test.py
+++ app/tests/db_test.py
@@ -1,17 +1,15 @@
 import unittest
-import os
+from unittest.mock import patch  
 from app.database import database_setup
-from unittest.mock import MagicMock
 
 class TestDatabase(unittest.TestCase):
     @patch("app.database.database_setup.connect")
     def testConnection(self, mock_connect):
-        if os.getenv("CI"):
+        if os.getenv("CI"):  
             mock_connect.return_value = "mock_db"  # Fake a successful connection
         else:
             connection = database_setup.connect()
             self.assertNotEqual(connection, -1, "Connection is not working")
-
 
 if __name__ == "__main__":
     unittest.main()
